df5566c6065df5516a31a68d3be0e9c8b4ea8966
Romain DECLE <115003401+homo-metallicus@users.noreply.github.com>
2022-10-05 10:07:44 +0200

    Add files via upload

---
+++ moOde-UI/setupDB.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+SQLDB=/var/local/www/db/moode-sqlite3.db
+
+LAST_ID=$(sqlite3 ${SQLDB} "SELECT id FROM cfg_system ORDER BY id DESC LIMIT 1;")
+ID=$(($LAST_ID+1))
+sqlite3 ${SQLDB} "INSERT INTO cfg_system VALUES ('"${ID}"','mount_status', '0');"
+
+exit 0

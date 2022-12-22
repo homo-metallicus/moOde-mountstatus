ef403266f08234e4c3dd7f3bd7f4b27f3cd89534
Romain DECLE <115003401+homo-metallicus@users.noreply.github.com>
2022-12-22 14:30:54 +0100

    Update setupDB.sh

--- moOde-UI/setupDB.sh
+++ moOde-UI/setupDB.sh
@@ -4,6 +4,6 @@ SQLDB=/var/local/www/db/moode-sqlite3.db
 
 LAST_ID=$(sqlite3 ${SQLDB} "SELECT id FROM cfg_system ORDER BY id DESC LIMIT 1;")
 ID=$(($LAST_ID+1))
-sqlite3 ${SQLDB} "INSERT INTO cfg_system VALUES ('"${ID}"','mount_status', '0');"
+sqlite3 ${SQLDB} "INSERT INTO cfg_system VALUES ('"${ID}"','mount_status', '0');" && echo "OK" || echo "error"
 
 exit 0
